03b6b61217be74c50bdfcfae92c10546c7300f24
DavidH1982 <david.houghton1982@gmail.com>
2022-02-07 21:35:33 +0000

    restart

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+import "./App.css";
+// import {BrowserRouter as Router, Switch, Link, Route} from "react-router-dom";
+
+import Register from "./components/register";
+import Login from "./components/login";
+
+function App() {
+  const baseURL = `${process.env.BASE_URL}`;
+
+  return (
+    <div className="App">
+      <Register />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
